ca9bacc93f7acb707c16fa041c6d7e68d1ad7603
thebrianzeng <thebrianzeng@gmail.com>
2014-10-18 19:59:10 -0400

    added url shortener and started nytimes

--- src/com/example/android/softkeyboard/Bot.java
+++ src/com/example/android/softkeyboard/Bot.java
@@ -1,8 +1,16 @@
 package com.example.android.softkeyboard;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Bot {
 	private String name;
-	
+	private static final String BITLY_API_TOKEN = "66691df0b4df6faffe0c35b3eac990dc5df44e30";
+
 	public Bot(String input_name){
 		name = input_name;
 	}
@@ -25,13 +33,26 @@ public class Bot {
 		return name;
 	}
 
-	private static String shortenURL(String input_url){
+	public static String shortenURL(String input_url){
 		/**
-		 * take in a url and shorten it using the bit.ly or google or whatever url shortening service
+		 * Take in a url and shorten it using the bit.ly or google or whatever url shortening service
 		 */
-		String ouput_url= "";
-		
-		return ouput_url;
+		String bitly_base = "http://api-ssl.bitly.com/v3/shorten?access_token=" + BITLY_API_TOKEN;
+        String bitly_api_call = bitly_base + "longUrl=" + input_url;
+        ServiceHandler http = new ServiceHandler();
+        ArrayList<JSONObject> result = new ArrayList<JSONObject>();
+        String json_object;
+        json_object = http.makeCall(bitly_api_call, ServiceHandler.GET);
+        String output_url = "";
+        try {
+            JSONObject jsonObj = new JSONObject(json_object);
+            JSONObject data = jsonObj.getJSONObject("data");
+            output_url = data.getString("url");
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return output_url;
 	}
 	
 	private static int fishPlayRandom(int min, int max){
