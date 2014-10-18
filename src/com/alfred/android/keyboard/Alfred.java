e3d815dc007c353ba8f516c2fff46a41fc5b94a9
Niger Little-Poole <nl2418@columbia.edu>
2014-10-18 13:04:17 -0700

    // Alfred Class
    initiated Alfred Class with Alfred object and methods

---
+++ src/com/alfred/android/keyboard/Alfred.java
@@ -0,0 +1,35 @@
+package com.alfred.android.keyboard;
+
+public class Alfred {
+	private String name;
+	
+	public Alfred(String input_name){
+		name = name;
+	}
+	
+	public String request(String input_request){
+		String response= "";
+		
+		return response;
+	}
+
+	private static String shortenURL(String input_url){
+		/**
+		 * take in a url and shorten it using the bit.ly or google or whatever url shortening service
+		 */
+		String ouput_url= "";
+		
+		return ouput_url;
+	}
+	
+	private static int fishPlayRandom(int min, int max){
+		/**
+		 * Query fishPlayPokemon api to get position, then normalize to some double on interval [0,1]
+		 * multiply double by difference of the bounds and then add to the min for a random variable on 
+		 * input interval
+		 */
+		return 0;
+	}
+	
+	
+}
