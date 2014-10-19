2e5e235fdd2c1f363ae6a9a95e58f2b4610434af
Niger Little-Poole <nl2418@columbia.edu>
2014-10-18 18:58:31 -0700

    animate me
    Works with multiword query support added to Inform me

--- src/com/example/android/softkeyboard/Giphy.java
+++ src/com/example/android/softkeyboard/Giphy.java
@@ -1,43 +1,43 @@
 package com.example.android.softkeyboard;
 
-import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NYTimes {
+import android.net.Uri;
 
-    private static final String API_KEY = "4ad554f8aa8c4626d8b260745f101857:10:70023015";
+public class Giphy {
 
-    public static String getArticle(String query) {
-    	try {
+	
+    public static String getGif(String query) {
+        String base_url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=";
+        try {
 			query = URLEncoder.encode(query, "utf-8");
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		};
-        String base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + API_KEY + "&";
-        String request_url = base_url + "q=" + query;
+        String request_url = base_url  + query;
 
         ServiceHandler http = new ServiceHandler();
         String json_object;
         json_object = http.makeCall(request_url, ServiceHandler.GET);
-        String output_url = "";
+        String output_url = "No gifs found";
         try {
             JSONObject jsonObj = new JSONObject(json_object);
-            JSONArray data = jsonObj.getJSONObject("response").getJSONArray("docs");
-//            int random = Bot.fishPlayRandom(0, 5);
-            JSONObject page = (JSONObject) data.get(0);
-            output_url = page.getString("web_url");
+            JSONArray data = jsonObj.getJSONArray("data");
+           int random = Bot.fishPlayRandom(0, data.length());
+            JSONObject image = (JSONObject) data.get(random);
+            output_url = image.getString("embed_url");
         } catch (JSONException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return output_url;
+        finally{
+        	return output_url;
+        }
     }
-
 }
